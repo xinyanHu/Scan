08f474a2a439556d93c313c5f68461d5b266a3a1
huxinyan <anonymous@torzo.club>
2018-05-06 15:08:36 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 胡欣妍 on 2018/5/6.
+//  Copyright © 2018年 胡欣妍. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by 胡欣妍 on 2018/5/6.
+//  Copyright © 2018年 胡欣妍. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
